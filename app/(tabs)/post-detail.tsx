bc70349e8c98df4b1b5286048d877e643caf15ff
muhiqbalm <muhiqbalmasykuri@gmail.com>
2025-07-25 10:07:52 +0700

    feat: init post detail

--- app/(tabs)/post-detail.tsx
+++ app/(tabs)/post-detail.tsx
@@ -1,10 +1,15 @@
 // PRODUCT DETAIL (RIO)
 
+import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 const URL = "https://dummyjson.com/posts/1";
 const URL_COMMENT_BY_POSTID = "https://dummyjson.com/comments/post/6";
 
 export default function PostDetail() {
-  return <ThemedView></ThemedView>;
+  return (
+    <ThemedView style={{ height: "100%" }}>
+      <ThemedText>POST DETAIL</ThemedText>
+    </ThemedView>
+  );
 }
